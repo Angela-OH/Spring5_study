858af9a6beb383f3dad07c301d3eff4d211e840d
Angela-OH <wisdom990629@likelion.org>
2022-10-18 22:06:38 +0900

    221018

--- sp5-chap11/src/main/java/config/MvcConfig.java
+++ sp5-chap11/src/main/java/config/MvcConfig.java
@@ -3,10 +3,10 @@ package config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// spring MVC 설정 파일
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
@@ -17,8 +17,16 @@ public class MvcConfig implements WebMvcConfigurer {
 		configurer.enable();
 	}
 	
+	// ViewResolver 설정을 추가
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/view/", ".jsp");
 	}
+	
+	// 요청 경로와 View 이름만 연결해주는 단순 연결에 사용 (특별한 logic x)
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/main").setViewName("main");
+	}
+
 }
